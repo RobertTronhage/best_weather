6ec242e68e5f98cf4ff706229ad9e2a6a3955bce
RobertTronhage <143345075+RobertTronhage@users.noreply.github.com>
2024-05-15 14:26:44 +0200

    initial commit

---
+++ src/main/java/se/norrland/best_weather/BestWeatherApplication.java
@@ -0,0 +1,13 @@
+package se.norrland.best_weather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestWeatherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BestWeatherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/norrland/best_weather/BestWeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package se.norrland.best_weather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestWeatherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
