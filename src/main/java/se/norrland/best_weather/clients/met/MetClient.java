9b8f7f78ae12ea95759a2d5f64b19837c9259893
MagNording <magnus_nording@msn.com>
2024-05-15 16:13:16 +0200

    MetClient

--- src/main/java/se/norrland/best_weather/clients/met/MetClient.java
+++ src/main/java/se/norrland/best_weather/clients/met/MetClient.java
@@ -1,6 +1,28 @@
 package se.norrland.best_weather.clients.met;
 
-public class MetClient {
-    //Klient för MET
-    // https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=59.3110&lon=18.0300
+import se.norrland.best_weather.clients.ForecastHandler;
+import se.norrland.best_weather.clients.domain.Humidity;
+import se.norrland.best_weather.clients.domain.Temperature;
+import se.norrland.best_weather.clients.domain.WeatherInfo;
+
+public class MetClient implements ForecastHandler {
+
+    private static final String GET_URI = "https://api.met.no/weatherapi/locationforecast/2.0/" +
+            "compact?lat=59.3110&lon=18.0300";
+
+
+    @Override
+    public Temperature getTemperature() {
+        return null;
+    }
+
+    @Override
+    public Humidity getHumidity() {
+        return null;
+    }
+
+    @Override
+    public WeatherInfo makeForecast() {
+        return null;
+    }
 }
