88786c150957cf79362773937d9ec05035b92f96
RobertTronhage <143345075+RobertTronhage@users.noreply.github.com>
2024-05-16 14:03:39 +0200

    structure

--- src/main/java/se/norrland/best_weather/clients/smhi/SmhiClient.java
+++ src/main/java/se/norrland/best_weather/clients/smhi/SmhiClient.java
@@ -1,16 +1,20 @@
 package se.norrland.best_weather.clients.smhi;
 
+import org.springframework.boot.autoconfigure.web.reactive.function.client.WebClientSsl;
+import org.springframework.stereotype.Component;
 import se.norrland.best_weather.clients.domain.Humidity;
 import se.norrland.best_weather.clients.domain.Temperature;
+import org.springframework.web.reactive.function.client.WebClient;
 import se.norrland.best_weather.clients.domain.WeatherInfo;
 import se.norrland.best_weather.clients.ForecastHandler;
 
+@Component
 public class SmhiClient implements ForecastHandler {
 
 
-    private static final String URL = "https://opendata-download-metfcst.smhi.se/";
-    private static final String GET_GEO = "/api/category/pmp3g/version/2/geotype/point/lon" +
-            "/18.0300/lat/59.3110/data.json";
+    private static final String URL = "https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/18.0300/lat/59.3110/data.json";
+
+    WebClient client = WebClient.create(URL);
 
     @Override
     public Temperature getTemperature() {
@@ -24,6 +28,11 @@ public class SmhiClient implements ForecastHandler {
 
     @Override
     public WeatherInfo makeForecast() {
-        return null;
+        WeatherInfo weatherInfo = client.get()
+                .retrieve()
+                .bodyToMono(WeatherInfo.class)
+                .block();
+
+        return weatherInfo;
     }
 }
